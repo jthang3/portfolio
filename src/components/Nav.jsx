fdfa8e6e51e99b5d59fbb6bebf44bbb2e41ca0a4
jothang <jthang3@gmail.com>
2022-02-08 11:50:36 -0500

    menu items

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,22 +6,34 @@ const Nav = () => {
     const openMenu = () => {
         let addClass = document.querySelector(".menu-btn");
         let getNav = document.querySelector(".nav-bar");
+        let getMenu = document.querySelector(".menu--options__close");
         if(open === false) {
             addClass.classList.add("open");
             getNav.classList.add("nav-bar__width");
+            getMenu.classList.add("menu--options__open");
             setOpen(true);
         }
         else {
             addClass.classList.remove("open");
             getNav.classList.remove("nav-bar__width");
+            getMenu.classList.remove("menu--options__open");
             setOpen(false);
         }
     }
     return(
+        <div>
             <div className="menu-btn" onClick={openMenu}>
-                <div className="menu-btn__burger">        
+                    <div className="menu-btn__burger">     
+                    </div>
             </div>
-            </div>
+            <div className="menu--options">
+                    <ul className="menu--options__close">
+                        <li>Home</li>
+                        <li>About Me</li>
+                        <li>Skills</li>
+                    </ul>   
+                </div>
+        </div>
     )
 }
 
